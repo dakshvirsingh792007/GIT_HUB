c4167cb2cba5585ac0f2326d187646e6820cfa49
Dashvir Singh <dakshvirsingh8m7967@gmail.com>
2025-12-16 11:49:25 +0530

    First

---
+++ Fib.py
@@ -0,0 +1,7 @@
+a=1
+b=1
+for i in range(10):
+    c=a+b
+    print(c,end=" ")
+    a=b
+    b=c
---
+++ Git_Commands.py
@@ -0,0 +1,22 @@
+print("Helllo")
+
+"""
+cd .. parent folder
+cd. child folder
+
+
+In the Command Line (Linux/Windows)
+cd (no arguments): Changes to your home directory, the default starting point for your user.
+cd .: Stays in the current directory. It's generally redundant but confirms you're there.
+cd ..: Moves you up one level to the parent directory.
+cd /: Changes to the root directory (top of the file system). 
+
+ls 
+
+The git init command is used to create a new, empty Git repository.
+git init  
+
+
+
+
+"""
